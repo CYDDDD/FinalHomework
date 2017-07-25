a60b753730ed382f2ea5a7b06f03170149c7b817
CYDDDD <1095775100@qq.com>
2017-07-25 16:21:23 +0800

    1

--- StartHomework/src/main/java/cn/edu/cumt/xxxt/Service/GoodService.java
+++
@@ -1,9 +0,0 @@
-package cn.edu.cumt.xxxt.Service;
-
-import org.apache.ibatis.annotations.Param;
-
-public interface GoodService {
-	void addGood(String good_name,String good_num,Integer price,String catagory_sn,Integer count);
-	void changeGood(String good_name,String good_num,Integer price,String catagory_sn,Integer count);
-	void deleteGood(String good_name,String good_num,Integer price,String catagory_sn,Integer count);
-}
